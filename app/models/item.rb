9175fbc1a1f633b55199b81a96f4906c27070349
kazuma4869 <zumakun4869@gmail.com>
2020-09-29 17:13:15 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   blong_to :user
+
+  validates :name, presence :true, length: { maximum:40}
+  validates :explain, presence :true, length: { maximum:1000}
+  validates :price, presence :true, numericality: { greater_than:300,less_than:999999},format: /\A[0-9]+\z/
+  validates :categor_id, presence :true
+  validates :status_id, presence :true
+  validates :fee_id, presence :true
+  validates :prefecture_id, presence :true
+  validates :shipment_id, presence :true
 end
