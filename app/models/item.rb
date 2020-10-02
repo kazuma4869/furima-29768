cb5c30f2c273e1a52ea4302245a740cfdb94b5dc
kazuma4869 <zumakun4869@gmail.com>
2020-10-02 22:13:36 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,18 +5,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipment
+  has_one_attached :image
   belongs_to :user
 
-  #validates :name, length: { maximum:40}
-  #validates :explain, presence :true, length: { maximum:1000}
-  #validates :price, presence :true, numericality: { greater_than:300,less_than:999999},format: /\A[0-9]+\z/
-  #validates :status_id, numericality: { other_than: 1 } 
-  #validates :categor_id, numericality: { other_than: 1 } 
-  #validates :fee_id, numericality: { other_than: 1 } 
-  #validates :prefecture_id, numericality: { other_than: 1 } 
-  #validates :shipment_id, numericality: { other_than: 1 } 
+  validates :name, length: { maximum:40}
+  validates :explain, length: { maximum:1000}
+  validates :price, numericality: { greater_than:300,less_than:999999},format: /\A[0-9]+\z/
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :fee_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :shipment_id, numericality: { other_than: 1 } 
   #空の投稿を保存できないようにする
   validates :name, :explain, :price, presence: true
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :categor_id, numericality: { other_than: 1 } 
+  #ジャンルの選択が「--」の時は保存できないようにする 
 end
