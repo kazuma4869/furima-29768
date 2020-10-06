87686ce28e70c7fcd20bb1ee7b32c8f71ea9f863
kazuma4869 <zumakun4869@gmail.com>
2020-10-06 20:14:03 +0900

    コントローラーの設定

--- app/controllers/pre_controller.rb
+++ app/controllers/pre_controller.rb
@@ -1,9 +1,21 @@
 class PreController < ApplicationController
   before_action :move_to_index
 
-  
+  def update
+    if @item.update(pre_params)
+      redirect_to root_path
+    else
+      render redirect_to item_path(@item.id)
+    end
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def pre_params
+    params.require(:pre).permit(:name).merge(user_id: current_user.id)
+  end
 end
