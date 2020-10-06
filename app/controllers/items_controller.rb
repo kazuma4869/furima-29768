8d2ec33ff3962aae00a3b728f40dca19c485e2fb
kazuma4869 <zumakun4869@gmail.com>
2020-10-06 16:42:48 +0900

    修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      redirect_to item_path
+      redirect_to item_path(item.id)
     end
   end
 
