0ae0b2cbcbcedf0e8438017d623cfe3cc672db7d
kazuma4869 <zumakun4869@gmail.com>
2020-10-06 20:14:14 +0900

    バリデーションの設定

--- app/models/pre.rb
+++ app/models/pre.rb
@@ -2,4 +2,16 @@ class Pre < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :management
   belongs_to_active_hash :prefecture
+
+  #validates :name, length: { maximum:40}
+  #validates :explain, length: { maximum:1000}
+  #validates :price, numericality: { greater_than:300,less_than:999999},format: /\A[0-9]+\z/
+  #validates :status_id, numericality: { other_than: 1 } 
+  #validates :category_id, numericality: { other_than: 1 } 
+  #validates :fee_id, numericality: { other_than: 1 } 
+  #validates :prefecture_id, numericality: { other_than: 1 } 
+  #validates :shipment_id, numericality: { other_than: 1 } 
+  #空の投稿を保存できないようにする
+  #validates :name, :explain, :price, presence: true
+  #ジャンルの選択が「--」の時は保存できないようにする
 end
