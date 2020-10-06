437123dbe721d88bfc46a48e836501bb00565101
kazuma4869 <zumakun4869@gmail.com>
2020-10-06 19:31:10 +0900

    migrate

---
+++ db/migrate/20201006075707_create_pres.rb
@@ -0,0 +1,14 @@
+class CreatePres < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pres do |t|
+      t.string :house_code,           null:false
+      t.integer :prefecture_id,       null:false
+      t.string :city,                 null:false
+      t.string :house_address,        null:false
+      t.string :house_name,           null:false
+      t.string :telephone,            null:false
+      t.references :management,       null:false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
