f4536b2df1edb5b2aee1d0aaf68a3c477d71ef71
kazuma4869 <zumakun4869@gmail.com>
2020-10-06 20:13:41 +0900

    テストコード２

--- spec/factories/pres.rb
+++ spec/factories/pres.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
-  factory :pre do
-    
+  factory :item do
+    house_code              {"666-6666"}
+    prefecture_id           {"2"}
+    city                    {"大川市"}
+    house_address           {"000"}
+    telephone               {"000-0000-0000"}
+    association :management
   end
 end
