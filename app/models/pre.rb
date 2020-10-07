2d8faf4501e21b233bb3a5ba26d887411e1d22ee
kazuma4869 <zumakun4869@gmail.com>
2020-10-07 22:09:44 +0900

    バリデーション追加

--- app/models/pre.rb
+++ app/models/pre.rb
@@ -5,15 +5,13 @@ class Pre < ApplicationRecord
   attr_accessor :token
 
   validates :token, presence: true
-  #validates :name, length: { maximum:40}
-  #validates :explain, length: { maximum:1000}
-  #validates :price, numericality: { greater_than:300,less_than:999999},format: /\A[0-9]+\z/
-  #validates :status_id, numericality: { other_than: 1 } 
-  #validates :category_id, numericality: { other_than: 1 } 
-  #validates :fee_id, numericality: { other_than: 1 } 
-  #validates :prefecture_id, numericality: { other_than: 1 } 
-  #validates :shipment_id, numericality: { other_than: 1 } 
-  #空の投稿を保存できないようにする
-  #validates :name, :explain, :price, presence: true
-  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :number, presence: true
+  validates :exp_month, presence: true
+  validates :exp_year, presence: true
+  validates :cvc, presence: true
+  validates :house_code presence: true
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :city, presence: true
+  validates :house_address, presence: true
+  validates :telephone, presence:true
 end
