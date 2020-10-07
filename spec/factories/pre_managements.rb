d3460200ad721891cb2d9bd511a57f8637a3d59a
kazuma4869 <zumakun4869@gmail.com>
2020-10-07 22:10:36 +0900

    テストコード追加

---
+++ spec/factories/pre_managements.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :pre_management do
+    
+  end
+end
--- spec/factories/pres.rb
+++ spec/factories/pres.rb
@@ -2,9 +2,11 @@ FactoryBot.define do
   factory :pres do
     house_code              {"666-6666"}
     prefecture_id           {"2"}
-    city                    {"大川市"}
+    city                    {"丸々市"}
     house_address           {"000"}
-    telephone               {"000-0000-0000"}
+    telephone               {"00000000000"}
+    price                   {3000}
+    token                   {"tok_abcdefghijk00000000000000000"}
     association :management
   end
 end
