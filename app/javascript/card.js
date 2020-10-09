622ff13d81f2eee9d7f53b3130816b1f0ba67915
kazuma4869 <zumakun4869@gmail.com>
2020-10-09 16:32:22 +0900

    JavaScript修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = ()=> {
-  Payjp.setPublicKey("pk_test_63e3b82da2bdfa11ecb0c393");　// PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -7,17 +7,19 @@ const pay = ()=> {
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
+      number: formData.get("pre_management[number]"),
+      cvc: formData.get("pre_management[cvc]"),
+      exp_month: formData.get("pre_management[exp_month]"),
+      exp_year: `20${formData.get("pre_management[exp_year]")}`,
     };
-
+    
     Payjp.createToken(card, (status, response) => {
+      console.log(status)
+      console.log(response)
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        const tokenObj = `<input value=${token} type="hidden" name='pre_management[token]'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
       document.getElementById("number").removeAttribute("name");
