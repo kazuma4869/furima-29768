15946bc386ec466d6868b44e069db6884ae87b3b
kazuma4869 <zumakun4869@gmail.com>
2020-10-09 16:32:35 +0900

    バリデーション追加

--- app/models/pre.rb
+++ app/models/pre.rb
@@ -1,17 +1,6 @@
 class Pre < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  #extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :management
-  belongs_to_active_hash :prefecture
-  attr_accessor :token
+  #belongs_to_active_hash :prefecture
 
-  validates :token, presence: true
-  validates :number, presence: true
-  validates :exp_month, presence: true
-  validates :exp_year, presence: true
-  validates :cvc, presence: true
-  validates :house_code presence: true
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :city, presence: true
-  validates :house_address, presence: true
-  validates :telephone, presence:true
 end
